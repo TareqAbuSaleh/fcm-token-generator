6bc1a945f78668c862b3519fe211677ad0205a4c
tareq abu saleh <hhgg_tareq@hotmail.com>
2025-08-01 02:20:00 +0400

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD-Yg7dS2G8DdNklALxQ-MwvWn0fFSZfuU",
+    authDomain: "id-trust-c57d4.firebaseapp.com",
+    projectId: "id-trust-c57d4",
+    storageBucket: "id-trust-c57d4.firebasestorage.app",
+    messagingSenderId: "9150909726",
+    appId: "1:9150909726:android:a67df27975aad1ca048d0f"
+  }
+};
