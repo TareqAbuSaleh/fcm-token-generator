0da5d4b7f3d56eb803cf2e60d02a0ebf7f754f37
tareq abu saleh <hhgg_tareq@hotmail.com>
2025-08-01 02:59:15 +0400

    Fix package name mismatch between google-services.json and app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fcm-token-generator',
-  webDir: 'www'
+  appName: 'FCM Token Generator',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
